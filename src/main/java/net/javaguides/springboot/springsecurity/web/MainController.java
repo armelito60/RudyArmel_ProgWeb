080ca91ef496a9efbcee935dea714b8552464ba5
armelito60 <tchiasso@et.esiea.fr>
2020-11-28 15:25:15 +0100

    liste pseudo terminée et commencement table event

--- src/main/java/net/javaguides/springboot/springsecurity/web/MainController.java
+++ src/main/java/net/javaguides/springboot/springsecurity/web/MainController.java
@@ -1,16 +1,10 @@
 package net.javaguides.springboot.springsecurity.web;
 
-import net.javaguides.springboot.springsecurity.model.User;
 import net.javaguides.springboot.springsecurity.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 
 @Controller
 public class MainController {
@@ -31,24 +25,6 @@ public class MainController {
     @GetMapping("/user")
     public String userIndex() {
         return "user/index";
-    }
-
-    @GetMapping("/survey")
-    public String stepOneSurvey(Model model) {
-
-        List<User> userList = userService.getAllByPseudo();
-        List<String> pseudoList = new ArrayList<>();
-
-        for (User user: userList) {
-            pseudoList.add(user.getPseudo());
-        }
-
-        Collections.sort(pseudoList);
-        System.out.println(pseudoList);
-        System.out.println(userList);
-
-        model.addAttribute("pseudo", pseudoList);
-        return "survey";
     }
 
 }
